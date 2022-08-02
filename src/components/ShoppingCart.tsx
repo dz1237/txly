b9dc2d411b7176856c4b98157933ad2cb7a35e32
dz1237 <2366987785@qq.com>
2022-08-02 11:35:04 +0800

    事件处理

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './ShoppingCart.module.css';
+import { FiShoppingCart } from 'react-icons/fi';
 interface Props {
 
 }
@@ -14,16 +15,20 @@ class ShoppingCart extends React.Component<Props, State> {
             isOpen: false
         };
     }
+    handleClick = (e: any) => {
+        if ((e.target as HTMLElement).nodeName === "SPAN") {
+            this.setState({
+                isOpen: !this.state.isOpen
+            })
+        }
+
+    }
     render() {
         return (
             <div className={styles.cartContainer}>
                 <button className={styles.button}
-                    onClick={() => {
-                        this.setState({
-                            isOpen: !this.state.isOpen
-                        })
-                    }}
-                >购物车</button>
+                    onClick={this.handleClick}
+                ><FiShoppingCart /><span>购物车</span></button>
                 <div className={styles.cartDropDown}
                     style={{ display: this.state.isOpen ? 'block' : 'none' }}
                 >
