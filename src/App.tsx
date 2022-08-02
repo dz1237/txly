70a2b46474ca76612d87d04d63cf40b79f0e81e4
dz1237 <2366987785@qq.com>
2022-08-02 11:48:25 +0800

    获取Api

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,36 @@ import robots from './mockdata/robots.json'
 import Robot from './components/Robot';
 import styles from './App.module.css'
 import ShoppingCart from './components/ShoppingCart'
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="" className= {styles.appLogo}/>
-        <h1>qweqwdsafwedfwerqww</h1>
+interface Props{}
+interface State{
+  robotGallery:any
+}
+class App extends React.Component<Props, State> {
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      robotGallery:[]
+    }
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response=>response.json())
+    .then(data=>this.setState({robotGallery:data}))
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="" className={styles.appLogo} />
+          <h1>qweqwdsafwedfwerqww</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r:any)=> <Robot id={r.id} email={r.email} name={r.name} />)}
+        </div>
       </div>
-      <ShoppingCart/>
-      <div className={styles.robotList}>
-        {robots.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
